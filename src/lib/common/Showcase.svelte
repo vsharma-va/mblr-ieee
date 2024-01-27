15ee4a418ff6d0ab60d57de8986aaafe8fc11ce7
Vaibhav Sharma <yavsvaibhav@gmail.com>
2024-01-27 20:15:25 +0530

    converted showcase into a standalone component

---
+++ src/lib/common/Showcase.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let showcaseHeading;
+    export let smallVersion;
+</script>
+
+<div class="h-[550vh] bg-on-surface w-full">
+    <div class="flex flex-col w-full h-[500px] gap-5 sticky top-[2%] lg:top-[10%] xl:top-[20%] items-center justify-center showcase-pictures-div px-[5%]">
+        <div class="flex flex-col items-start w-full md:w-[90%] lg:w-[55%] justify-center">
+            <p class="heading-font text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-primary-container">{showcaseHeading}</p>
+        </div>
+        {#if !smallVersion}
+            <div class="flex flex-col h-1/2 sm:h-[70%] w-full 2xl:h-[85%] md:w-[90%] lg:w-[55%] bg-surface rounded-2xl relative items-center justify-center">
+                <div class="absolute -bottom-1/2 sm:-bottom-1/3 sm:right-0 md:-right-16 lg:-right-52 lg:top-[12.5%] bg-on-surface border-4 border-border/30 rounded-2xl h-full sm:h-[65%] lg:h-[80%] w-[95%] sm:w-[65%] lg:w-[65%] z-[1]">
+                    <slot name="inside-side-card"/>
+                </div>
+                <slot name="inside-main-card"/>
+            </div>
+        {:else}
+            <div class="flex flex-col h-1/2 sm:h-[70%] w-full 2xl:h-[85%] md:w-[90%] lg:w-[55%] bg-surface rounded-2xl relative items-center justify-center">
+                <div class="absolute -bottom-[35%] sm:-bottom-[18%] sm:right-0 md:-right-16 lg:-right-52 lg:top-[35%] bg-on-surface border-4 border-border/30 rounded-2xl h-1/2 sm:h-[35%] w-[95%] sm:w-[65%] lg:w-[65%] z-[1]">
+                    <slot name="inside-side-card"/>
+                </div>
+                <slot name="inside-main-card"/>
+            </div>
+        {/if}
+    </div>
+</div>
