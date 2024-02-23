d62cff3d298abea683cb5d42acc25958a9f3d8c3
Vaibhav Sharma <yavsvaibhav@gmail.com>
2024-02-24 01:37:38 +0530

    added width property to be animated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      colors:{
-        "surface": "#fafdfd",
-        "border": "#808080",
-        "on-surface": "#191c1d",
-        "primary": "#006874",
-        "on-primary": "#ffffff",
-        "primary-container": "#96f0ff",
-        "on-primary-container": "#001f24",
-        "cis-primary-color": "#b02d2c",
-        "cis-on-primary-color": "#ffffff",
-        "cis-primary-container": "#ffdad6",
-        "cis-on-primary-container": "#410003",
-        "cis-surface": "#fffbff",
-        "cis-on-surface": "#201a19",
-        "cs-primary": "#676000",
-        "cs-on-primary": "#ffffff",
-        "cs-primary-container": "#f1e66b",
-        "cs-on-primary-container": "#1f1c00",
-        "cs-surface": "#fffbff",
-        "cs-on-surface": "#1d1c16",
-      }
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            transitionProperty: {
+                'width': 'width',
+            },
+            colors: {
+                "surface": "#fafdfd",
+                "border": "#808080",
+                "on-surface": "#191c1d",
+                "primary": "#006874",
+                "on-primary": "#ffffff",
+                "primary-container": "#96f0ff",
+                "on-primary-container": "#001f24",
+                "cis-primary-color": "#b02d2c",
+                "cis-on-primary-color": "#ffffff",
+                "cis-primary-container": "#ffdad6",
+                "cis-on-primary-container": "#410003",
+                "cis-surface": "#fffbff",
+                "cis-on-surface": "#201a19",
+                "cs-primary": "#676000",
+                "cs-on-primary": "#ffffff",
+                "cs-primary-container": "#f1e66b",
+                "cs-on-primary-container": "#1f1c00",
+                "cs-surface": "#fffbff",
+                "cs-on-surface": "#1d1c16",
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
