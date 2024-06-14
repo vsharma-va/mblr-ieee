5bc9dec83f9184d2cb3cc08d47e3a74f201880b1
Vaibhav Sharma <yavsvaibhav@gmail.com>
2024-06-15 01:02:20 +0530

    working home page - some animations

---
+++ src/lib/common/Loader.svelte
@@ -0,0 +1,145 @@
+<script>
+    import {gsap} from "gsap/dist/gsap";
+    import {onMount} from "svelte";
+
+    let textAnimationDuration = 1;
+
+    onMount(() => {
+        let textCaptureTimeline = gsap.timeline({
+            onComplete: () => {
+                textCapturePartTwoTimeline.play(0);
+            },
+        });
+        let textCapturePartTwoTimeline = gsap.timeline({
+            onComplete: () => {
+                textCapturePartThreeTimeline.play(0);
+            },
+        });
+        let textCapturePartThreeTimeline = gsap.timeline();
+        textCapturePartThreeTimeline.pause(0);
+        textCapturePartTwoTimeline.pause(0);
+        // textCaptureTimeline.pause(0);
+        for (let i = 0; i < 6; i++) {
+            textCaptureTimeline.to(
+                `.down-text-master-${i + 1}`,
+                {
+                    y: -((i + 1) * 250),
+                    ease: "power4.inOut",
+                    duration: textAnimationDuration,
+                },
+                "<",
+            );
+            textCaptureTimeline.to(
+                `.down-text-your-${i + 1}`,
+                {
+                    y: 0,
+                    ease: "power4.inOut",
+                    duration: textAnimationDuration,
+                },
+                "<",
+            );
+            textCapturePartTwoTimeline.to(
+                `.down-text-your-${i + 1}`,
+                {
+                    y: -((i + 1) * 250),
+                    ease: "power4.inOut",
+                    duration: textAnimationDuration,
+                },
+                "<",
+            );
+            textCapturePartTwoTimeline.to(
+                `.down-text-karma-${i + 1}`,
+                {
+                    y: 0,
+                    ease: "power4.inOut",
+                    duration: textAnimationDuration,
+                },
+                "<",
+            );
+            textCapturePartThreeTimeline.to(
+                `.down-text-karma-${i + 1}`,
+                {
+                    y: -((i + 1) * 250),
+                    ease: "power4.inOut",
+                    duration: textAnimationDuration,
+                },
+                "<",
+            );
+        }
+        textCapturePartThreeTimeline.to(
+            ".div-up-1",
+            {
+                yPercent: -150,
+                ease: "power4.inOut",
+                duration: 1.2,
+            },
+            "<0.15",
+        );
+        textCapturePartThreeTimeline.to(
+            ".div-up-2",
+            {
+                yPercent: -200,
+                ease: "power4.inOut",
+                duration: 1.2,
+            },
+            "<",
+        );
+        textCapturePartThreeTimeline.to(
+            ".div-up-3",
+            {
+                yPercent: -250,
+                ease: "power4.inOut",
+                duration: 1.2,
+            },
+            "<",
+        );
+        textCapturePartThreeTimeline.to('.loading-animation-main', {
+            display: 'none',
+        });
+    });
+</script>
+
+<div class="w-full h-screen fixed top-0 content-center grid grid-cols-3 loading-animation-main z-[4]">
+    <div class="col-start-1 col-end-1 div-up-1 bg-surface h-full w-full absolute top-0"></div>
+    <div class="col-start-2 col-end-2 div-up-2 bg-surface h-full w-full absolute top-0"></div>
+    <div class="col-start-3 col-end-3 div-up-3 bg-surface h-full w-full absolute top-0"></div>
+    <div
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row w-full"
+    >
+        <div
+                class="text-9xl text-center w-full h-full primary-font tracking-wide scale-125 flex flex-row items-center justify-center overflow-hidden master-text"
+        >
+            <span class="down-text-master-1">M</span>
+            <span class="down-text-master-2">A</span>
+            <span class="down-text-master-3">S</span>
+            <span class="down-text-master-4">T</span>
+            <span class="down-text-master-5">E</span>
+            <span class="down-text-master-6">R</span>
+        </div>
+    </div>
+    <div
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex flex-row"
+    >
+        <div
+                class="text-9xl text-center w-full h-full text-primary primary-font tracking-wide flex flex-row items-center justify-center overflow-hidden your-text"
+        >
+            <span class="down-text-your-1 translate-y-[150px]">Y</span>
+            <span class="down-text-your-2 translate-y-[400px]">O</span>
+            <span class="down-text-your-3 translate-y-[800px]">U</span>
+            <span class="down-text-your-4 translate-y-[1200px]">R</span>
+        </div>
+    </div>
+    <div
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex flex-row"
+    >
+        <div
+                class="text-9xl text-center w-full h-full text-primary primary-font tracking-wide flex flex-row items-center justify-center overflow-hidden your-text"
+        >
+            <span class="down-text-karma-1 translate-y-[150px]">K</span>
+            <span class="down-text-karma-2 translate-y-[400px]">A</span>
+            <span class="down-text-karma-3 translate-y-[800px]">R</span>
+            <span class="down-text-karma-4 translate-y-[1200px]">M</span>
+            <span class="down-text-karma-4 translate-y-[1600px]">A</span>
+        </div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,11 @@
     import {onMount} from "svelte";
     import {gsap} from "gsap/dist/gsap";
     import {TextPlugin} from 'gsap/dist/TextPlugin';
+    import RSVP from "$lib/home/RSVP.svelte";
+    import Loader from "$lib/common/Loader.svelte";
 
     let currentTime;
+    let mobileRSVP;
     let currentLoadingPercentage = 0;
     let cycleBranchName = gsap.timeline({
         onComplete: () => {
@@ -23,6 +26,7 @@
     let percentageInterval;
     onMount(() => {
         gsap.registerPlugin(TextPlugin);
+
         onLoadTimeline.to('.main-navbar', {
             scale: 1,
             ease: "power4.inOut",
@@ -57,16 +61,26 @@
                 duration: 0.75,
             }, '<');
         }
-        onLoadTimeline.to('.rsvp-container', {
-            bottom: '7rem',
-            left: '6rem',
-            ease: 'power4.inOut',
-        }, '<0.1');
-        onLoadTimeline.to('.rsvp-container', {
-            rotation: '12deg',
-            duration: 1.5,
-            ease: 'bounce.out',
-        }, '>');
+        if (mobileRSVP) {
+            onLoadTimeline.to('.mobile-rsvp', {
+                scale: 1,
+                opacity: 1,
+                display: 'flex',
+                duration: 0.5,
+                ease: 'power4.inOut',
+            }, '<0.1');
+            onLoadTimeline.to('.mobile-rsvp-stamp', {
+                opacity: 1,
+                scale: 1,
+                ease: 'power4.inOut',
+                duration: 0.75,
+            });
+            onLoadTimeline.to('.mobile-rsvp', {
+                rotate: '12deg',
+                duration: 0.75,
+                ease: 'bounce.out',
+            }, '>');
+        }
 
         cycleBranchName.to('.landing-branch-name', {
             text: "IEEE",
@@ -120,9 +134,18 @@
     }
 </script>
 
+<Loader />
 <div class="h-fit min-h-screen w-full bg-surface">
     <div class="h-screen bg-primary-container w-full relative">
         <div class="h-screen w-full flex flex-col items-center justify-center scale-[0.85] bg-surface landing-page-container relative p-2 overflow-hidden pb-24 pt-12 md:hidden">
+            <div class="flex flex-col absolute -left-[42%] top-0 w-fit h-fit gap-5">
+                <div class="w-[100vw] -rotate-45 h-5 bg-primary-container"></div>
+                <div class="w-[100vw] -rotate-45 h-5 bg-primary-container"></div>
+            </div>
+            <div class="flex flex-col absolute -right-[42%] bottom-0 w-fit h-fit gap-2 -rotate-45">
+                <div class="w-[100vw] h-5 bg-primary-container"></div>
+                <div class="w-[100vw] h-5 bg-primary-container"></div>
+            </div>
             <div class="w-[80%] sm:w-[45%] h-fit py-2 rounded-t-xl flex flex-col items-center justify-center bg-on-surface overflow-hidden -rotate-3 origin-bottom-left">
                 <div class="w-full h-fit p-2 flex flex-row items-center justify-center relative">
                     <p class="text-2xl primary-font text-surface capitalize text-center flex flex-row items-center justify-center">
@@ -134,54 +157,61 @@
                         style="border-style: none none dotted; border: 2px #0e1418 dashed">
                 </div>
             </div>
-            <div class="w-[78%] sm:w-[45%] h-screen max-h-[500px] py-2 rounded-b-xl flex flex-col bg-on-surface overflow-hidden z-[3]">
-                <div class="w-full h-full relative flex flex-col">
+            <div class="w-[78%] sm:w-[45%] h-screen max-h-[500px] py-2 rounded-b-xl flex flex-col bg-on-surface z-[3]">
+                <div class="w-full h-full relative flex flex-col items-center justify-center">
                     <div class="h-6 w-5 absolute -top-[0.95rem] -right-[0.75rem] rounded-full bg-surface z-[3]"></div>
                     <div class="h-6 w-5 absolute -top-[0.95rem] -left-[0.75rem] rounded-full bg-surface z-[3]"></div>
                     <div class="h-full w-full flex flex-col items-center justify-center gap-1 px-4">
+                        <p class="text-2xl text-center primary-font text-surface">
+                            YOU HAVE BEEN <span class="brand-font">Invited!</span>
+                        </p>
                         <div class="text-8xl font-bold primary-font flex flex-row text-primary-container text-center h-fit w-fit overflow-hidden landing-branch-name">
                             <span class="landing-branch-letter-1 translate-y-[100px]">I</span><span
                                 class="landing-branch-letter-2 translate-y-[500px]">E</span><span
                                 class="landing-branch-letter-3 translate-y-[900px]">E</span><span
                                 class="landing-branch-letter-4 translate-y-[1300px]">E</span>
                         </div>
-                        <div class="text-8xl font-bold primary-font text-primary-container -mt-4 text-center flex flex-row overflow-hidden">
-                            <span class="landing-society-letter-1 translate-y-[100px]">S</span><span
-                                class="landing-society-letter-2 translate-y-[400px]">O</span><span
-                                class="landing-society-letter-3 translate-y-[500px]">C</span><span
-                                class="landing-society-letter-4 translate-y-[900px]">I</span><span
-                                class="landing-society-letter-5 translate-y-[1300px]">E</span><span
-                                class="landing-society-letter-6 translate-y-[1700px]">T</span><span
-                                class="landing-society-letter-7 translate-y-[2100px]">Y</span>
+                        <div class="text-8xl font-bold primary-font text-primary-container -mt-4 text-center flex flex-col">
+                            <div class="w-fit h-fit flex flex-row overflow-hidden">
+                                <span class="landing-society-letter-1 translate-y-[100px]">S</span><span
+                                    class="landing-society-letter-2 translate-y-[400px]">O</span><span
+                                    class="landing-society-letter-3 translate-y-[500px]">C</span><span
+                                    class="landing-society-letter-4 translate-y-[900px]">I</span><span
+                                    class="landing-society-letter-5 translate-y-[1300px]">E</span><span
+                                    class="landing-society-letter-6 translate-y-[1700px]">T</span><span
+                                    class="landing-society-letter-7 translate-y-[2100px]">Y</span>
+                            </div>
+                            <div class="h-fit w-[105%] bg-tertiary-container text-on-tertiary-container -rotate-3 origin-bottom-left py-1 px-4 flex items-center justify-center landing-student-ribbon">
+                                <p class="text-3xl font-bold primary-font">
+                                    STUDENT BRANCH
+                                </p>
+                            </div>
                         </div>
-                        <!--                                                <p class="text-8xl font-bold primary-font text-primary-container text-center">IEEE<br>SOCIETY-->
-                        <!--                                                </p>-->
-                        <div class="h-fit w-[85%] bg-tertiary-container text-on-tertiary-container -rotate-3 origin-bottom-left py-1 px-4 flex items-center justify-center landing-student-ribbon">
-                            <p class="text-3xl font-bold primary-font">
-                                STUDENT BRANCH
-                            </p>
-                        </div>
-                        <p class="text-5xl font-bold primary-font text-surface mt-5">WELCOME</p>
-                        <p class="text-3xl font-bold primary-font text-surface">ADMIT X1</p>
-                        <!--                        <div class="w-full h-[100px] flex flex-col items-center justify-center object-contain">-->
-                        <!--                            <img src="{barcodeImg}" alt="dummy barcode" class="h-full w-full object-cover">-->
-                        <!--                        </div>-->
+                        <RSVP bind:this={mobileRSVP}/>
                     </div>
                 </div>
             </div>
         </div>
         <div class="h-screen w-full hidden md:visible md:flex flex-col items-center justify-center scale-[0.85] bg-surface landing-page-container relative p-2 overflow-hidden pb-28 pt-4 px-4">
-            <div class="h-full w-[85%] flex flex-row items-center justify-start z-[2]">
-                <div class="p-7 h-[55%] bg-on-surface rounded-l-xl w-fit flex flex-row items-center justify-center relative overflow-hidden -rotate-1 origin-bottom-right">
+            <div class="flex flex-col absolute -left-[42%] top-0 w-fit h-fit gap-5">
+                <div class="w-[100vw] -rotate-45 h-5 bg-primary-container"></div>
+                <div class="w-[100vw] -rotate-45 h-5 bg-primary-container"></div>
+            </div>
+            <div class="flex flex-col absolute -right-[42%] bottom-0 w-fit h-fit gap-2 -rotate-45">
+                <div class="w-[100vw] h-5 bg-primary-container"></div>
+                <div class="w-[100vw] h-5 bg-primary-container"></div>
+            </div>
+            <div class="min-h-fit w-[85%] min-[900px]:w-[70%] lg:w-[65%] xl:w-[55%] 2xl:w-[47%] flex flex-row items-center justify-start z-[2]">
+                <div class="p-7 h-full bg-on-surface rounded-l-xl w-fit flex flex-row items-center justify-center relative overflow-hidden -rotate-1 origin-bottom-right">
                     <div class="h-full w-fit relative flex flex-col items-center justify-between">
                         <div class="h-fit w-fit flex flex-col items-center justify-center">
-                            <p class="text-2xl primary-font text-surface capitalize text-center flex flex-row items-center justify-center"
+                            <p class="text-2xl lg:text-3xl primary-font text-surface capitalize text-center flex flex-row items-center justify-center"
                                style="writing-mode: sideways-lr">
                                 ADMIT X1
                             </p>
                         </div>
                         <div class="h-fit w-fit flex items-center justify-center">
-                            <p class="text-2xl primary-font text-surface capitalize text-center flex flex-row items-center justify-center"
+                            <p class="text-2xl lg:text-3xl primary-font text-surface capitalize text-center flex flex-row items-center justify-center"
                                style="writing-mode: sideways-lr">
                                 YOU HAVE BEEN <span class="brand-font text-2xl">Invited!</span>
                             </p>
@@ -191,19 +221,19 @@
                     <div class="h-6 w-5 absolute -right-[0.65rem] -bottom-[0.75rem] bg-surface rounded-full"></div>
                     <div class="absolute right-0 bg-surface w-[0.5px] h-full border-[0.5px] border-surface border-dashed"></div>
                 </div>
-                <div class="h-[55%] w-full bg-on-surface rounded-r-xl relative flex flex-col items-center justify-center pb-4 px-4">
+                <div class="w-full bg-on-surface rounded-r-xl relative flex flex-col items-center justify-center pb-4 px-4">
                     <div class="h-6 w-5 absolute -left-[0.65rem] -top-[0.75rem] bg-surface rounded-full"></div>
                     <div class="h-6 w-5 absolute -left-[0.65rem] -bottom-[0.75rem] bg-surface rounded-full"></div>
-                    <p class="text-2xl primary-font text-surface capitalize text-center flex flex-row items-center justify-center">
+                    <p class="text-2xl lg:text-3xl primary-font text-surface capitalize text-center flex flex-row items-center justify-center">
                         CONNECTING <span class="brand-font text-2xl">Ideas</span>, CREATING <span
                             class="brand-font text-2xl">Futures</span></p>
-                    <div class="text-[6.5rem] font-bold primary-font flex flex-row text-primary-container text-center h-fit w-fit overflow-hidden landing-branch-name leading-[1]">
+                    <div class="text-[6.5rem] lg:text-9xl font-bold primary-font flex flex-row text-primary-container text-center h-fit w-fit overflow-hidden landing-branch-name leading-[1]">
                         <span class="landing-branch-letter-1 translate-y-[100px]">I</span><span
                             class="landing-branch-letter-2 translate-y-[500px]">E</span><span
                             class="landing-branch-letter-3 translate-y-[900px]">E</span><span
                             class="landing-branch-letter-4 translate-y-[1300px]">E</span>
                     </div>
-                    <div class="text-[6.5rem] font-bold primary-font text-primary-container -mt-4 text-center flex flex-row overflow-hidden leading-[1]">
+                    <div class="text-[6.5rem] lg:text-9xl font-bold primary-font text-primary-container -mt-4 text-center flex flex-row overflow-hidden leading-[1]">
                         <span class="landing-society-letter-1 translate-y-[100px]">S</span><span
                             class="landing-society-letter-2 translate-y-[400px]">O</span><span
                             class="landing-society-letter-3 translate-y-[500px]">C</span><span
@@ -213,16 +243,14 @@
                             class="landing-society-letter-7 translate-y-[2100px]">Y</span>
                     </div>
                     <div class="h-fit w-[65%] bg-tertiary-container text-on-tertiary-container -rotate-3 origin-bottom-left py-1 px-4 flex items-center justify-center landing-student-ribbon">
-                        <p class="text-3xl font-bold primary-font">
+                        <p class="text-3xl lg:text-4xl font-bold primary-font">
                             STUDENT BRANCH
                         </p>
                     </div>
+                    <div class="absolute top-[88%] left-1/2 -translate-x-1/2">
+                        <RSVP bind:this="{mobileRSVP}"/>
+                    </div>
                 </div>
-            </div>
-            <div class="w-fit h-fit absolute bottom-1/2 left-1/2 bg-primary-container rounded-xl p-4 -rotate-[20deg] origin-top-left rsvp-container">
-                <p class="text-3xl text-on-primary-container primary-font">RSVP</p>
-                <p class="text-2xl text-on-primary-container primary-font">YES LETS SCROLL</p>
-                <p class="text-2xl text-on-primary-container primary-font">EHM I AM ALREADY SOLD! BUT SURE</p>
             </div>
         </div>
 
